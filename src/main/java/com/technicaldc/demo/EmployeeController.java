cdeb9287f45f5d37404349ea9ce14f3e234d1aa9
Dilip Chauhan <chauhandilip305@gmail.com>
2025-06-14 02:48:54 +0530

    base rest api

--- src/main/java/com/technicaldc/demo/EmployeeController.java
+++ src/main/java/com/technicaldc/demo/EmployeeController.java
@@ -1,8 +1,14 @@
 package com.technicaldc.demo;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class EmployeeController {
@@ -12,5 +18,24 @@ public class EmployeeController {
 	@GetMapping("/")
 	public Employees getEmployees() {
 		return employeeDAO.getAllEmployees();
+	}
+
+	@PostMapping("/")
+	public ResponseEntity<Object> addEmployee(@RequestBody Employee employee) {
+		Integer newId = employeeDAO
+			.getAllEmployees()
+			.getEmployeeList()
+			.size() + 1;
+
+		employee.setId(newId);
+		employeeDAO.addEmployee(employee);
+
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(employee.getId())
+			.toUri();
+
+		return ResponseEntity.created(location).build();
 	}
 }
