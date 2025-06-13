cc1f8d71e905ffd1fe83231d1a3f9fd08a46f153
Dilip Chauhan <chauhandilip305@gmail.com>
2025-06-13 21:27:59 +0530

    initial commit

---
+++ src/main/java/com/technicaldc/demo/SoftwareEngineer.java
@@ -0,0 +1,15 @@
+package com.technicaldc.demo;
+
+public class SoftwareEngineer{
+	private Integer id;
+	private String name;
+	private String techStack;
+
+	public SoftwareEngineer() { }
+
+	public SoftwareEngineer(Integer id, String name, String techStack) {
+		this.id = id;
+		this.name = name;
+		this.techStack = techStack;
+	}
+}
